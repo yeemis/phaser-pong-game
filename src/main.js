85ddfd815e8256674ced733ca35b6a071fc80d52
Ryan Theodore The <ryan.the.2006@gmail.com>
2025-01-23 09:13:09 +0000

    Update template

--- src/main.js
+++ src/main.js
@@ -9,12 +9,6 @@ const config = {
     height: 768,
     parent: 'game-container',
     backgroundColor: '#000',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false
-        }
-    },
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
