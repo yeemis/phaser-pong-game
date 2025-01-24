a7372ff5055dd368c75e762a8ea2aa0cb296aa6e
Ryan Theodore The <ryan.the.2006@gmail.com>
2025-01-24 08:56:03 +0000

    Update for cp1

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,12 +6,25 @@ const HEIGHT = 768;
 export class Game extends Scene {
     constructor() {
         super('Game');
+        // Initialise necessary variables
+        this.ball = null;
+        this.leftPaddle = null;
+        this.rightPaddle = null;
     }
 
     preload() {
+        // Load necessary assets from the assets directory
+        this.load.image('background', 'assets/background.png');
+        this.load.image('ball', 'assets/ball.png');
+        this.load.image('paddle', 'assets/paddle.png');
     }
 
     create() {
+        // Add background, ball, and paddles to the scene
+        this.add.image(WIDTH/2, HEIGHT/2, 'background').setScale(0.8, 0.8);
+        this.ball = this.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0.05, 0.05);
+        this.leftPaddle = this.add.image(50, 384, "paddle");
+        this.rightPaddle = this.add.image(974, 384, "paddle");
     }
 
     update() {
